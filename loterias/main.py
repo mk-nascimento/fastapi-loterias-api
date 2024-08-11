0c738a836eeab81b274f8207b3c03d0af7a141dd
Maksuel Nascimento <114680477+mk-nascimento@users.noreply.github.com>
2024-08-11 22:46:34 +0000

    refactor: improve error handling and consistency
    - Standardize HTTP status code usage for better clarity across the application
    - Enhance error messages to provide more informative and user-friendly feedback
    - Suggest the latest available draw when a specific one is not found
    - Add model validation for database-provided entities against enumerated lottery draws

--- loterias/main.py
+++ loterias/main.py
@@ -1,5 +1,5 @@
 import logging
-from http import HTTPStatus
+from http import HTTPStatus as status
 
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import HTMLResponse, JSONResponse
@@ -20,29 +20,33 @@ app.include_router(admin.router)
 
 @app.exception_handler(Exception)
 def exception_handler(_: Request, exc: Exception):
-    INTERNAL = HTTPStatus.INTERNAL_SERVER_ERROR
+    INTERNAL = status.INTERNAL_SERVER_ERROR
     return JSONResponse({'detail': INTERNAL.phrase}, INTERNAL)
 
 
-@app.get('/', include_in_schema=False, status_code=HTTPStatus.OK)
+@app.get('/', include_in_schema=False, status_code=status.OK)
 def index():
     return HTMLResponse("Visit Swagger documentation at: <a href='/docs'>/docs</a>")
 
 
-@app.get('/{loteria:str}', response_model=LotteryDraw, status_code=HTTPStatus.OK, tags=['Loteria'])
+@app.get('/{loteria:str}', response_model=LotteryDraw, status_code=status.OK, tags=['Loteria'])
 async def get_latest_lottery(*, session: Session, cache: Cache, loteria: Lottery):
     if cached := cache.get_cached_draw(loteria):
         return cached.model_dump()
     elif entity := await session.db[loteria.value].find_one(sort=[('concurso', -1)]):
-        return LotteryDraw.model_validate(entity)
-    raise HTTPException(HTTPStatus.NOT_FOUND, HTTPStatus.NOT_FOUND.phrase)
+        return LotteryDraw.model_validate(entity).model_dump()
+    raise HTTPException(status.NOT_FOUND, status.NOT_FOUND.phrase)
 
 
-@app.get('/{loteria:str}/{concurso:int}', response_model=LotteryDraw, status_code=HTTPStatus.OK, tags=['Loteria'])
+@app.get('/{loteria:str}/{concurso:int}', response_model=LotteryDraw, status_code=status.OK, tags=['Loteria'])
 async def get_lottery(*, session: Session, cache: Cache, loteria: Lottery, concurso: int):
     if not (isinstance(concurso, int) and len(str(concurso)) == 4):  # noqa: PLR2004
-        raise HTTPException(HTTPStatus.BAD_REQUEST, '`concurso` parameter must be a 4-digit integer.')
+        raise HTTPException(status.BAD_REQUEST, '`concurso` parameter must be a 4-digit integer.')
 
-    if (entity := cache.get_cached_draw(loteria)) is not None and entity.concurso == concurso:
-        return entity.model_dump()
-    return await session.db[loteria.value].find_one(dict(concurso=concurso))
+    if (cached := cache.get_cached_draw(loteria)) is not None:
+        if concurso != cached.concurso:
+            raise HTTPException(status.NOT_FOUND, {concurso: status.NOT_FOUND.phrase, 'latest': cached.concurso})
+        return cached.model_dump()
+    elif entity := await session.db[loteria.value].find_one(dict(concurso=concurso)):
+        return LotteryDraw.model_validate(entity).model_dump()
+    raise HTTPException(status.NOT_FOUND, f'#{concurso} {status.NOT_FOUND.phrase}')
